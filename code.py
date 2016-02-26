d68f903e0feeb26664f0143b23c46ad67122d956
KG001 HOMEDESK <82158824@qq.com>
2016-02-26 21:26:35 +0800

    extract first url

--- code.py
+++ code.py
@@ -1,5 +1,11 @@
-page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
-
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
-print start_link
+
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote + 1)
+
+url = page[start_quote+1:end_quote]
+
+print start_quote, end_quote
+print url
